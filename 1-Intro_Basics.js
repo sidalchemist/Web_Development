00e7322dc7020745400a1fc7490b0d93a39d354f
sidalchemist <41266525+sidalchemist@users.noreply.github.com>
2021-07-14 11:22:36 +0530

    Create 1-Intro_Basics.js

---
+++ 1-Intro_Basics.js
@@ -0,0 +1,17 @@
+//NODEJS 
+//OPEN SOURCE CROSS PLATFORM JS runtime env and library for running web apps outside the 
+//client browser
+//NPM -Node Package manager(Meaning in the name itself)
+
+//Installation checks:
+node --version
+npm --version
+
+//Short intro Prog
+
+setInterval(
+  ()=>{
+    console.log("Hello World!!")
+  },1000)
+
+//Req -nodejs
