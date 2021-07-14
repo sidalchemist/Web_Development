68a6b14c5215eb2c4cdc1982f0248fd48b44eeef
sidalchemist <41266525+sidalchemist@users.noreply.github.com>
2021-07-14 11:28:15 +0530

    Update 1-Intro_Basics.js

--- 1-Intro_Basics.js
+++ 1-Intro_Basics.js
@@ -9,9 +9,15 @@ npm --version
 
 //Short intro Prog
 
-setInterval(
-  ()=>{
-    console.log("Hello World!!")
-  },1000)
+const amount = 9
+
+if (amount < 10) {
+  console.log('small number')
+} else {
+  console.log('large number')
+}
+
+console.log(`hey it's my first node app!!!`)
+
 
 //Req -nodejs
